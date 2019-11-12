5d8acdedcdc2f4f5db10d819bd725face78709c9
coder-emre <emrekiser@gmail.com>
2019-11-12 14:28:24 +0300

    End of section 3

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,5 +1,5 @@
-using Microsoft.EntityFrameworkCore;
-using DatingApp.API.Model;
+using Microsoft.EntityFrameworkCore; 
+using DatingApp.API.Models;
 
 namespace DatingApp.API.Data
 {
@@ -8,5 +8,6 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options)  {  }
 
         public DbSet<Value> Values {get;set;}
+        public DbSet<User> Users{get;set;} 
     }
 }
